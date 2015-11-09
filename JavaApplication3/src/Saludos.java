25202d43d568772e258fb6879605b98584c01c00
Jeff <Jeff@DeJeffo>
2015-11-08 22:02:57 -0500

    Pequeño programa que devuelve un saludo luego de ingresar un nombre de ususario

---
+++ JavaApplication3/src/Saludos.java
@@ -0,0 +1,11 @@
+ import java.util.*;
+ //pequeño programa que permite ingresar un nombre y devuelve un saludo con el nombre ingresado
+ public class Saludos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String cadena;
+        System.out.println("Introduce un nombre: ");
+        cadena = sc.nextLine();
+        System.out.println("Buenos Días " + cadena);
+    }
+}
